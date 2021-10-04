93cc6b89557d3c7e192bda7c456ca9f7e72d94a0
Robert Wilson <robertwilson875@gmail.com>
2021-10-04 11:35:37 -0700

    got the append to work

--- COMP4537/labs/3/WriteFile/write.js
+++ COMP4537/labs/3/WriteFile/write.js
@@ -1,16 +1,18 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-http.createServer(function (req, res) {
-    const q = url.parse(req.url, true);
-    const filename = "." + q.pathname;
-    fs.appendFile(filename, function (err, data) {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            return res.end(q.pathname + " 404 Not Found!");
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(`${myQuery.params['text']}`);
-        return res.end();
-    });
-}).listen(8888);
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+http
+  .createServer(function (req, res) {
+    const myQuery = url.parse(req.url, true);
+    const filename = '.' + myQuery.pathname;
+    fs.appendFile(filename, `${myQuery.query.text} <br>` + '\n', function (err) {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        return res.end(filename + ' 404 Not Found!');
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(`wrote some data === ${myQuery.query.text}`);
+      return res.end();
+    });
+  })
+  .listen(3000);
