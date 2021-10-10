3957d45e0edbf8b37bff3dbcd5240e8e4e9fe645
Robert Wilson <robertwilson875@gmail.com>
2021-10-10 13:30:44 -0700

    need to fix send for post

--- COMP4537/labs/4/Server/app.js
+++ COMP4537/labs/4/Server/app.js
@@ -1,10 +1,37 @@
 const http = require('http');
+const url = require('url');
+const GET = "GET";
+const POST = "POST";
+const endPointRoot = "http://localhost:8085/";
+
+let dictionary = [];
 
 http.createServer(function (req, res) {
-  console.log("The Server recived your request");
   res.writeHead(200, {
     "Content-Type": "text/html",
-    "Access-Control-Allow-Origin": "*"
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*"
   });
-  res.end("server's response!");
+
+  if (req.method == GET) {
+    const q = url.parse(req.url, true);
+    console.log(q.query["word"]);
+    console.log("The Server recived your request");
+
+  };
+
+
+  if (req.method == POST) {
+    console.log(req);
+    const obj = JSON.parse(req.url);
+    console.log(obj);
+    dictionary.push(obj);
+    console.log(dictionary);
+    req.on('end', function () {
+      res.end("server response was a SUCCESS");
+
+        });
+
+  };
+
 }).listen(8085);
