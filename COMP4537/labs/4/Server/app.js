721271dbfb669106d4f76530a550b513c5325513
Robert Wilson <robertwilson875@gmail.com>
2021-10-16 19:26:39 -0700

    just need to host and css maybe

--- COMP4537/labs/4/Server/app.js
+++ COMP4537/labs/4/Server/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const GET = "GET";
 const POST = "POST";
-const endPointRoot = "/API/V1/";
+const endPointRoot = "/api/definitions/";
 const resource = "myDictionary/";
 
 let dictionary = [];
@@ -26,8 +26,7 @@ http.createServer(function (req, res) {
       console.log(dictionaryObject);
       res.end(`${dictionaryObject.word}: ${dictionaryObject.definition}`);
     } else {
-      res.end(`Request #: ${requestNum}<br>, word "${word}" not found!"`) \
-      requestNum += 1;
+      res.end(`Request #: ${requestNum}<br> The word: "${word}" was not found!"`);
     }
 
   };
@@ -38,6 +37,9 @@ http.createServer(function (req, res) {
     req.on('data', function (chunk) {
       if (chunk != null) {
         jsonObj = JSON.parse(chunk);
+
+      // message to notify word was saved.
+      res.end(`Request #: ${requestNum}<br> The word: ${jsonObj.word} was stored successfully!`);
       }
 
     console.log(typeof(jsonObj), jsonObj.word, jsonObj.definition);
@@ -46,10 +48,7 @@ http.createServer(function (req, res) {
       console.log("it exists");
 
       // If the word already exists, it returns a message. ( e.g. 'blah blah ' already exists.
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(`Request #: ${requestNum}<br>, ${jsonObj.word} already exists!`);
-      requestNum += 1;
-
+      res.end(`Request #: ${requestNum}<br> The word: ${jsonObj.word} already exists!`);
 
     } else {
 
@@ -62,17 +61,15 @@ http.createServer(function (req, res) {
       // New entry recorded:
       //"Book : A written or printed work consisting of pages glued or sewn together"
 
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(`Request #: ${requestNum}<br>, New Entry Recoreded: <br>" + ${jsonObj.word} ${jsonObj.definition}`);
-      requestNum += 1;
+      res.end(`Request #: ${requestNum}<br> New Entry Recoreded: <br> + Word:  ${jsonObj.word} <br> Defintion: ${jsonObj.definition}`);
     }
     });
-
     req.on('end', function () {
 
       res.end("Server Response: Hey! Your POST request was a great SUCCESS!");
         });
 
   };
+  requestNum += 1;
 
-}).listen(8085);
+}).listen(3000);
