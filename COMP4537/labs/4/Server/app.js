2a159d09825b4d65fc9fb0cde6e1d81f61893042
victorcho@outlook.com <BananaBread09!>
2021-10-17 14:25:39 -0700

    Optimization

--- COMP4537/labs/4/Server/app.js
+++ COMP4537/labs/4/Server/app.js
@@ -3,7 +3,6 @@ const url = require('url');
 const GET = "GET";
 const POST = "POST";
 const endPointRoot = "/api/definitions/";
-const resource = "myDictionary/";
 
 let dictionary = [];
 let requestNum = 0;
@@ -14,7 +13,6 @@ http.createServer(function (req, res) {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "*"
   });
-  console.log(req.headers);
 
   if (req.method == GET) {
     const q = url.parse(req.url, true);
@@ -23,7 +21,6 @@ http.createServer(function (req, res) {
 
     if (dictionary.some(dictionaryWord => dictionaryWord.word == word)) {
       let dictionaryObject = dictionary.find(dictionaryObject => dictionaryObject.word == word);
-      console.log(dictionaryObject);
       res.end(`${dictionaryObject.word}: ${dictionaryObject.definition}`);
     } else {
       res.end(`Request #: ${requestNum}<br> The word: "${word}" was not found!"`);
@@ -32,43 +29,23 @@ http.createServer(function (req, res) {
   };
 
 
-  if (req.method == POST && req.url === endPointRoot + resource) {
+  if (req.method == POST && req.url === endPointRoot) {
     let jsonObj;
     req.on('data', function (chunk) {
       if (chunk != null) {
         jsonObj = JSON.parse(chunk);
+        dictionary.push(jsonObj);
 
       // message to notify word was saved.
       res.end(`Request #: ${requestNum}<br> The word: ${jsonObj.word} was stored successfully!`);
-      }
 
-    console.log(typeof(jsonObj), jsonObj.word, jsonObj.definition);
-
-    if (dictionary.some(dictionaryWord => dictionaryWord.word == jsonObj.word)) {
+      } else if (dictionary.some(dictionaryWord => dictionaryWord.word == jsonObj.word)) {
       console.log("it exists");
 
       // If the word already exists, it returns a message. ( e.g. 'blah blah ' already exists.
       res.end(`Request #: ${requestNum}<br> The word: ${jsonObj.word} already exists!`);
-
-    } else {
-
-      dictionary.push(jsonObj);
-      console.log(dictionary, "true");
-
-      // Otherwise it adds the word and its definition to your dictionary and returns
-      // another appropriate message such as:
-      // Request # 102
-      // New entry recorded:
-      //"Book : A written or printed work consisting of pages glued or sewn together"
-
-      res.end(`Request #: ${requestNum}<br> New Entry Recoreded: <br> + Word:  ${jsonObj.word} <br> Defintion: ${jsonObj.definition}`);
     }
     });
-    req.on('end', function () {
-
-      res.end("Server Response: Hey! Your POST request was a great SUCCESS!");
-        });
-
   };
   requestNum += 1;
 
