18af89296ec9d25e33f482dbb8d734237a4ba457
Robert Wilson <robertwilson875@gmail.com>
2021-10-17 23:46:59 -0700

    done

--- COMP4537/labs/4/Server/app.js
+++ COMP4537/labs/4/Server/app.js
@@ -2,7 +2,6 @@ const http = require('http');
 const url = require('url');
 const GET = "GET";
 const POST = "POST";
-const endPointRoot = "/api/definitions/";
 
 let dictionary = {};
 let requestNum = 0;
@@ -11,43 +10,46 @@ http.createServer(function (req, res) {
   res.writeHead(200, {
     "Content-Type": "text/html",
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*"
   });
 
-  if (req.method === GET) {
+  if (req.method == GET) {
     requestNum = requestNum + 1;
     const qObject = url.parse(req.url, true);
-    console.log(qObject.query['word']);
 
-    if (qObject.query['word'] in dictionary) {
-      res.end(`Request #: ${requestNum}<br> Word: ${qObject.query['word']}<br> Definition: ${dictionary[qObject.query['word']]}`);
+    let myData = `${JSON.stringify(qObject.query.word)}`;
+
+    if (myData in dictionary) {
+      res.end(`Request #: ${requestNum}<br> Word: ${myData}<br> Definition: ${dictionary[myData]}`);
 
     } else {
-      res.end(`Request #: ${requestNum}<br> The word: "${qObject.query['word']}" was not found!"`);
+      res.end(`Request #: ${requestNum}<br> The word: ${myData} was not found!"`);
     }
+  }
 
-  };
-
-
-  if (req.method == POST && req.url === endPointRoot) {
+  if (req.method == POST) {
     let body = "";
+    let data = "";
     req.on('data', function (chunk) {
-      if (chunk != null) {
-        body = body + chunk;
-      }
+
+        body += chunk;
+        data = JSON.parse(body);
     });
 
     req.on('end', function() {
-      let qObject = url.parse(body, true);
+    const qObject = url.parse(req.url, true);
+
       requestNum = requestNum + 1;
-      if (qObject.query["word"] in dictionary) {
-        res.end(`Request #: ${requestNum}<br> The word: ${qObject.query['word']} already exists!`);
-      } else {
-        dictionary[qObject.query["word"]] = qObject.query["definition"];
-        res.end(`Request #: ${requestNum}<br> The word: ${qObject.query['word']} was stored successfully!`);
-        console.log(dictionary);
+      let myData = `${JSON.stringify(qObject.query.word)}`;
+      let myDeff = `${JSON.stringify(qObject.query.definition)}`;
+
+      if (myData in dictionary) {
+        res.end(`Request #: ${requestNum}<br> The word: ${myData} already exists!`);
+      }
+      else {
+        dictionary[myData] = myDeff;
+        // res.end(`${JSON.stringify(dictionary.myData)}`);
+        res.end(`Request #: ${requestNum}<br> The word: ${myData} was stored successfully!`);
     }
     });
-  };
-
-}).listen(3000);
+  }
+}).listen();
